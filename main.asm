55bb47a462881def9674940a4cff8a17be0c6f7b
Quied <quiedcpp@gmail.com>
2022-05-16 09:04:31 +0300

    some new

--- main.asm
+++ main.asm
@@ -6,6 +6,7 @@ _start:
 
 
 mov edx, len
+
 mov ecx, msg
 mov ebx, 1
 mov eax, 4
@@ -17,3 +18,16 @@ int 0x80
 section .data
 msg db 'Hello, World', 0xa
 len equ $ - msg
+
+mov eax, 1 ; sys_exit
+int 0x80 
+
+mov edx, 4   ; size of msg
+mov ecx, msg ; msg for out
+mov ebx, 1   ; file desriptor
+mov eax, 4   ; sys_write
+int 0x80     ; kernel
+
+
+
+
